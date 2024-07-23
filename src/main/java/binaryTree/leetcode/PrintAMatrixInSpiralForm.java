e46f58e5ec14bdefe5483e9716dd5d20b3c851e4
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-07-22 23:10:41 -0600

    Solved Print a Matrix in Spiral

--- src/main/java/binaryTree/leetcode/PrintAMatrixInSpiralForm.java
+++ src/main/java/binaryTree/leetcode/PrintAMatrixInSpiralForm.java
@@ -1,24 +1,42 @@
 package binaryTree.leetcode;
 
+import org.ietf.jgss.Oid;
+
 public class PrintAMatrixInSpiralForm {
     public static void spiralForm(int[][] matrix) {
-        int i = 0, r = matrix.length - 1, c = matrix[0].length - 1, k = 0, l = 0;
-        while(k < r && c < l) {
+        int i = 0, r = matrix.length, c = matrix[0].length, k = 0, l = 0;
+        while(k < r && l < c) {
             /*Left to right*/
             for(i = l; i < c; i++) {
                 System.out.print(matrix[k][i] + " ");
             }
-            
+            k++;
             /*Top to bottom*/
-            
-            
+            for (i = k; i < r; i++) {
+                System.out.print(matrix[i][c - 1] + " ");
+            }
+            c--; 
             /*Right to left*/
-            
+            if (k < r) {
+                for(i = c - 1; i >= l; i--) {
+                    System.out.print(matrix[ r - 1][i] + " ");
+                }
+                r--;
+            }
             /*Bottom to top*/
+            if (l < c) {
+                for(i = r - 1; i >= k; i--) {
+                    System.out.print(matrix[i][l] + " ");
+                }
+                l++;
+            }
         }
     }
     
     public static void main(String[] args) {
-        
+        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        int[][] matrix_2 = {{1}, {2}};
+        spiralForm(matrix);
+        spiralForm(matrix_2);
     }
 }
