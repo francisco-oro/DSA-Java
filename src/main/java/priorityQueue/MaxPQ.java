ff81c8272116b98d8d4aab08d361f24244a6f02f
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-07-23 01:17:20 -0600

    Added deleteMax() in MaxHea[

--- src/main/java/priorityQueue/MaxPQ.java
+++ src/main/java/priorityQueue/MaxPQ.java
@@ -27,16 +27,39 @@ public class MaxPQ {
     }
     
     private void swim(int k) {
-        while (k > 1 && heap[k/2] < heap[k]){ 
+        while (k > 1 && heap[k/2] < heap[k]) {
             int temp = heap[k];
-            heap[k] = heap[k/2];
-            heap[k/2] = temp;
-            k = k/2;
+            heap[k] = heap[k / 2];
+            heap[k / 2] = temp;
+            k = k / 2; // Continue shifting up till new value inserted is at corrected position
+        }
+    }
+    
+    public void resize(int newCapacity) {
+        int[] temp = new int[newCapacity];
+        System.arraycopy(heap, 0, temp, 0, n);
+        heap = temp;
+    }
+    
+    public void print() {
+        for (int i = 1; i <= n; i++) {
+            System.out.print(heap[i] + " ");
+        }
     }
     
     public static void main(String[] args) {
-        MaxPQ maxPQ = new MaxPQ(10);
-        System.out.println(maxPQ.isEmpty());
+        MaxPQ maxPQ = new MaxPQ(4);
+        maxPQ.insert(5);
+        maxPQ.insert(6);
+        maxPQ.insert(2);
+        maxPQ.insert(4);
+        maxPQ.print();
         System.out.println(maxPQ.size());
+        maxPQ.insert(6);
+        maxPQ.insert(1);
+        maxPQ.insert(3);
+        System.out.println(maxPQ.size());
+        maxPQ.print();
+        
     }
 }
